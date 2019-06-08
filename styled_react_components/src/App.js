ea8834d90ddfa603aa57f545bcd7d0bbe3f48715
hwanlee <ljhw3377@gmail.com>
2019-06-08 11:59:50 +0900

    css button

--- styled_react_components/src/App.js
+++ styled_react_components/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello React!
-    </div>
+    <Fragment>
+      <button className="button button--success">
+        Hello
+      </button>
+      <button className="button button--danger">
+        Hello
+      </button>
+    </Fragment>
   );
 }
 
