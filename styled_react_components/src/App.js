0dd31fc95e88f0e4b4bbb8e035f1e1be1d853dcf
hwanlee <ljhw3377@gmail.com>
2019-06-08 13:01:47 +0900

    Theming

--- styled_react_components/src/App.js
+++ styled_react_components/src/App.js
@@ -1,17 +1,13 @@
 import React, {Component, Fragment} from 'react';
-import styled, {createGlobalStyle, css} from "styled-components";
+import styled, {createGlobalStyle, ThemeProvider} from "styled-components";
+import theme from "./theme";
 
 createGlobalStyle`
   padding:0;
   margin:0;
 `
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`
+
 
 const Container = styled.div`
   height: 100vh;
@@ -19,21 +15,32 @@ const Container = styled.div`
   background-color: pink;
 `;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border:none;
-  ${awesomeCard}
+const Card = styled.div`
+  background-color: red;
 `
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Input placeholder="Hello"/>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form/>
+        </Container>
+      </ThemeProvider>
     );
   }
 }
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 export default App;
